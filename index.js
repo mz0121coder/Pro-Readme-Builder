dfb9507f30583b9644e7c37c6e36aacd17c367e2
Mohammad Zafar <sz7861@outlook.com>
2021-08-05 00:50:17 +0100

    add 4 main dependencies

---
+++ index.js
@@ -0,0 +1,5 @@
+// Dependencies: inquirer, file system, util module, markdown
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMarkdown = require("./utils/generateMarkdown");
