6eb7b3eebb0234f073bb984b5e9c33a6992f80e9
Mohammad Zafar <sz7861@outlook.com>
2021-08-05 00:53:39 +0100

    write file async, promises based on user responses

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
+// Write file async for promises based on successful user responses
+const writeFileAsync = util.promisify(fs.writeFile);
