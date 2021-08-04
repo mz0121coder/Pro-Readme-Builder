32680fcfb4f89eca2d4cb5f08b6d99f768a049ff
Mohammad Zafar <sz7861@outlook.com>
2021-08-05 00:57:24 +0100

    start questions array, prompt user for responses

--- index.js
+++ index.js
@@ -5,3 +5,40 @@ const util = require("util");
 const generateMarkdown = require("./utils/generateMarkdown");
 // Write file async for promises based on successful user responses
 const writeFileAsync = util.promisify(fs.writeFile);
+// Start questions array
+const questions = [
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "What is the URL?",
+      name: "url",
+    },
+    {
+      type: "input",
+      message: "Enter a description of your project",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "What are the installation instructions?",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "What is the usage information?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message: "Enter credits for your project",
+      name: "credits",
+    },
+    {
+      type: "input",
+      message: "Are there any test instructions for this project?",
+      name: "tests",
+    },
